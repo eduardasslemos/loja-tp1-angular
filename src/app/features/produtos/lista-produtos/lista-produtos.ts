482b8cd94633b74cb2c1691aed301fc34972c15f
eduardasslemos <eduardasslemos@gmail.com>
2026-09-17 22:06:00 -0300

    aula1709

--- src/app/features/produtos/lista-produtos/lista-produtos.ts
+++ src/app/features/produtos/lista-produtos/lista-produtos.ts
@@ -3,6 +3,7 @@ import { Produto } from '../../../model/produto';
 import { CardProduto } from "../card-produto/card-produto";
 import { ProdutoService } from '../services/produto.service';
 import { toSignal } from '@angular/core/rxjs-interop';
+import { finalize } from 'rxjs';
 
 @Component({
   selector: 'app-lista-produtos',
@@ -14,7 +15,9 @@ export class ListaProdutos {
 
   private produtoService = inject(ProdutoService);
 
-  private produtos = toSignal<Produto[], Produto[]>(this.produtoService.listar(),{initialValue: []});
+  public carregando = signal(true);
+
+  private produtos = toSignal<Produto[], Produto[]>(this.produtoService.listar().pipe(finalize(() => this.carregando.set(false))),{initialValue: []});
 
   apenasPromo = signal(false);
 
--- src/app/features/produtos/services/produto.service.ts
+++ src/app/features/produtos/services/produto.service.ts
@@ -1,13 +1,17 @@
 import { inject, Injectable } from '@angular/core';
 import { LoggerService } from '../../../core/services/logger/logger.service';
-import { Produto } from '../../../model/produto';
-import { delay, Observable, of } from 'rxjs';
+import { Produto, ProdutoMapper } from '../../../model/produto';
+import { catchError, delay, map, Observable, of } from 'rxjs';
+import { HttpBackend, HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProdutoService {
   private logger = inject(LoggerService);
+  private http = inject(HttpClient);
+
+  private apiUrl = 'https://fakestoreapi.com/products';
 
   private readonly listaMock = <Produto[]>[
     {
@@ -50,9 +54,13 @@ export class ProdutoService {
 
   listar(): Observable<Produto[]>{
     this.logger.info("[PRODUTO SERVICE] - Retornando lista de produtos")
-    return of(this.listaMock).pipe(
-      delay(250)
-    );
+    return this.http.get<any[]>(this.apiUrl).pipe(
+      map(lista => lista.map(prod => ProdutoMapper.fromJson(prod))),
+      catchError(erro => {
+        this.logger.error("[PRODUTO SERVICE] - Retornando lista de produtos");
+        return of([]);
+      })
+    )
   }
 
   getById(id: number): Observable<Produto | undefined>{
